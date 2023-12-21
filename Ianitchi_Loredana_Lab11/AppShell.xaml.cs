e909a124596389501ae08ea0c5c9c1a959666ae3
LOREDANA IANIȚCHI <loredana.ianitchi@stud.ubbcluj.ro>
2023-12-21 22:24:59 +0200

    Add project files.

---
+++ Ianitchi_Loredana_Lab11/AppShell.xaml.cs
@@ -0,0 +1,10 @@
+﻿namespace Ianitchi_Loredana_Lab11
+{
+    public partial class AppShell : Shell
+    {
+        public AppShell()
+        {
+            InitializeComponent();
+        }
+    }
+}
